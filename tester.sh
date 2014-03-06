9cd19f6564c06c8ab88526bed0090e37e68e74b2
Mick Tarsel <mtarsel@gmail.com>
2014-03-06 14:16:58 -0500

    Computes time and outputs properly t csv format. Adding thread counts

--- tester.sh
+++ tester.sh
@@ -10,11 +10,16 @@
 # grid size, thread count
 
 #
+# outputs .csv files in ./testing/thread-count or ./tesining/grid-size like so:
+#   [compile flag]_diff_[thread_count | grid_size].csv
 #
-#
+#For example:
+#   grid_diff_thread_count.csv 
+#   was compiled with grid size = 10, and executed with 1-10 thread count 
+#   with locking on grid
 #
 
-
+START=$(date +%s)
 
 mkdir -p ./testing/thread-count/
 mkdir -p ./testing/grid-size/
@@ -31,15 +36,13 @@ echo ""
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
 	./gridapp 10 $i -grid | grep elapsed | cut -d' ' -f3 >> ./testing/thread-count/grid_diff_thread_count.csv
-#	./gridapp 10 $i -grid > ./testing/thread-count/grid_thread_count_$i.txt
+	#Thanks to ddemarco5
 done
 
 #GRID: GRID TESTING
 for i in 2 3 4 5 6 7 8 9 10; do
 	echo "Executing with  grid size $i and 10 number of threads. "
-	#Thanks to ddemarco5
 	./gridapp $i 10 -grid | grep elapsed | cut -d' ' -f3 >> ./testing/grid-size/grid_diff_grid_size.csv
-	#./gridapp $i 10 -grid > ./testing/grid-size/grid_grid_size_$i.txt
 done
 
 
@@ -58,15 +61,13 @@ echo""
 #CELL : THREAD TESTING
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -cell > ./testing/thread-count/cell_thread_count_$i.txt
+	./gridapp 10 $i -cell | grep elapsed | cut -d' ' -f3 >> ./testing/thread-count/cell_diff_thread_count.csv
 done
 
 #CELL : GRID TESTING
 for i in 2 3 4 5 6 7 8 9 10; do
 	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -cell > ./testing/grid-size/cell_grid_size_$i.txt
+	./gridapp $i 10 -cell | grep elapsed | cut -d' ' -f3 >> ./testing/grid-size/cell_diff_grid_size.csv
 done
 
 
@@ -82,15 +83,13 @@ echo""
 #ROW : THREAD TESTING
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -row > ./testing/thread-count/row_thread_count_$i.txt
+	./gridapp 10 $i -row | grep elapsed | cut -d' ' -f3 >> ./testing/thread-count/row_diff_thread_count.csv
 done
 
 #ROW: GRID TESTING
 for i in 2 3 4 5 6 7 8 9 10; do
 	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -row > ./testing/grid-size/row_grid_size_$i.txt
+	./gridapp $i 10 -row | grep elapsed | cut -d' ' -f3 >> ./testing/grid-size/row_diff_grid_size.csv
 done
 
 
@@ -109,16 +108,16 @@ echo""
 #NONE : THREAD TESTING
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -none > ./testing/thread-count/none_thread_count_$i.txt
+	./gridapp 10 $i -none | grep elapsed | cut -d' ' -f3 >> ./testing/thread-count/none_diff_thread_count.csv
 done
 
 #NONE: GRID TESTING
 for i in 2 3 4 5 6 7 8 9 10; do
 	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -none > ./testing/grid-size/none_grid_size_$i.txt
+	./gridapp $i 10 -none | grep elapsed | cut -d' ' -f3 >> ./testing/grid-size/none_diff_grid_size.csv
 done
+
+END=$(date +%s)
 
 echo""
 echo""
@@ -129,6 +128,7 @@ echo " ./gridapp [1-10] 10 -none "
 echo "#####################"
 echo""
 echo""
-echo" done."
+DIFF=$(( $END - $START ))
+echo "It took $DIFF seconds"
 echo""
-
+echo" done."
