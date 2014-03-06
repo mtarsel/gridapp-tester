252f5b7330b76a18600b24f264a4a605a3945079
Mick Tarsel <mtarsel@gmail.com>
2014-03-06 13:48:18 -0500

    Added grep to output to csv, testing now

--- tester.sh
+++ tester.sh
@@ -10,16 +10,14 @@
 # grid size, thread count
 
 #
-#To compile and run:
-#    chomd u+x ./tester.sh
-#    time ./tester.sh
+#
 #
 #
 
 
 
-#mkdir -p ./testing/thread-count/
-#mkdir -p ./testing/grid-size/
+mkdir -p ./testing/thread-count/
+mkdir -p ./testing/grid-size/
 
 clear
 
@@ -32,15 +30,16 @@ echo ""
 #GRID : THREAD TESTING
 for i in 1 2 3 4 5 6 7 8 9 10; do
 echo "Executing with $i number of threads. Grid size 10 "
-#	touch ./testing/thread-count/thread_count_$i.txt
-	./gridapp 10 $i -grid > ./testing/thread-count/grid_thread_count_$i.txt
+	./gridapp 10 $i -grid | grep elapsed | cut -d' ' -f3 >> ./testing/thread-count/grid_diff_thread_count.csv
+#	./gridapp 10 $i -grid > ./testing/thread-count/grid_thread_count_$i.txt
 done
 
 #GRID: GRID TESTING
 for i in 2 3 4 5 6 7 8 9 10; do
 	echo "Executing with  grid size $i and 10 number of threads. "
-	#touch ./testing/gird-size/grid_size_$i.txt
-	./gridapp $i 10 -grid > ./testing/grid-size/grid_grid_size_$i.txt
+	#Thanks to ddemarco5
+	./gridapp $i 10 -grid | grep elapsed | cut -d' ' -f3 >> ./testing/grid-size/grid_diff_grid_size.csv
+	#./gridapp $i 10 -grid > ./testing/grid-size/grid_grid_size_$i.txt
 done
 
 
@@ -103,6 +102,8 @@ echo " ./gridapp 10 [1-10] -row "
 echo " ./gridapp [1-10] 10 -row " 
 echo "#####################"
 echo""
+echo""
+echo" done."
 echo""
 
 #NONE : THREAD TESTING
